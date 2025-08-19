ebd2a0a16ba869c8460a6ad06f6ec2af5fe52db2
Parker Knight <plb.knight@gmail.com>
2025-08-19 12:00:19 -0400

    Add functionality for by-week calculation

--- cooccurrence.R
+++ cooccurrence.R
@@ -1,8 +1,11 @@
 ### Compute the co-occurrence matrix
-getCooccur <- function(df, window) {
+getCooccur <- function(df, window, byPatient = FALSE) {
   codes <- unique(df$Code)
+  times <- unique(df$Time)
   p <- length(codes)
   cooccur <- matrix(0, p, p)
+  
+  if (byPatient) {
   for (j in 1:(p - 1)) {
     codej <- codes[j]
     timesj <- df[df$Code == codej,]$Time
@@ -16,9 +19,30 @@ getCooccur <- function(df, window) {
     }
     cooccur[j,j] <- length(timesj)
   }
+  
+  }
+  else { # | Code | Time | nOccurrences |
+    for (j in 1:(p - 1)) {
+      codej <- codes[j]
+      for (jj in (j+1):p)
+      {
+        codejj <- codes[jj]
+        n_cooccur_j.jj <- sapply(times, function(t){
+          out <- min(df[df$Code %in% c(codej, codejj),]$nOccurrences)
+          print(out)
+          out <- ifelse(length(out) == 0, 0, out)
+          return(out)
+        })
+        cooccur[j,jj] <- sum(n_cooccur_j.jj)
+      }
+      cooccur[j,j] <- sum(df[df$Code == codej, ]$nOccurrences)
+    }
+    cooccur[p,p] <- sum(df[df$Code == codes[p], ]$nOccurrences)
+    
+  }
   cooccur <- cooccur + t(cooccur)
   row.names(cooccur) <- codes
   colnames(cooccur) <- codes
-
+  
   return(cooccur)
 }
--- generateData.R
+++ generateData.R
@@ -1,7 +1,7 @@
 set.seed(32)
 library(dplyr)
 d <- 10
-rates <- 1 / sample(1:25, size = d)
+rates <- 15 / sample(1:25, size = d)
 n <- 10
 T <- 26
 df <- data.frame(PatientID = c("asdf"),
@@ -22,5 +22,26 @@ for (i in 1:n) {
 }
 df <- df[-1,]
 df <- filter(df, Time < T)
+df$Time <- round(df$Time)
 
-write.csv(x = df, file = "example.csv",row.names = FALSE)
+write.csv(x = df, file = "example_byPatient.csv",row.names = FALSE)
+
+times <- unique(df$Time)
+codes <- unique(df$Code)
+df.final <- data.frame(Code = c("1"),
+                       Time = c(0),
+                       nOccurrences = c(0))
+for (t in times) {
+  for (code in codes) {
+    df.cur <- df[(df$Code == code) & (df$Time == t),]
+    if (nrow(df.cur) > 0)
+      df.final <- rbind(df.final, data.frame(Code = code, Time = t, 
+                                             nOccurrences = nrow(df.cur)))
+  }
+}
+df.final <- df.final[-1,]
+
+write.csv(x = df.final, file = "example.csv", row.names = FALSE)
+
+source("cooccurrence.R")
+getCooccur(df.final)
